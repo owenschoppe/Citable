bd52b4015239e19e100a6f2f7a434783704817e3
Owen Schoppe <owen.schoppe@gmail.com>
2019-02-14 09:29:22 -0800

    feat(export) Format select menu

--- js/exportChicago.js
+++ js/exportChicago.js
@@ -1,167 +1,73 @@
 /*jshint esversion: 6 */
- function exportChicago(rows) {
+function exportChicago(rows) {
 
-  var website = function(strings, type, title, authors, dateAccessed, datePublished, url) {
+  function website(type, title, authors, dateAccessed, datePublished, url) {
+    //Check that we have the minimum number of attributes for a web citation.
+    //Publisher/Site/Organization name should go after the formatted title.
+    if(url && dateAccessed && (authors || title)) {
+      return `${formatAuthors(authors)} ${formatTitle(title)} ${ datePublished ? `Last modified ${formatDate(datePublished)}` : `Accessed ${formatDate(dateAccessed)}`}. ${formatURL(url)}\n\n`;
+    } else {
+      console.log('Insufficient information to cite:',type,title,authors,dateAccessed,datePublished,url);
+      return '';
+    }
+  }
+
+  function formatAuthors(authors) {
     //format authors: `last, first, and first last.` sorted by last name. If >10 authors, show 7 followed by `et al.`
-    //format dates: `Month DD, YYYY.`
-    return `${authors}. "${title}." ${ datePublished ? `Last modified ${datePublished}` : `Accessed ${dateAccessed}`}. ${url}.`;
-  };
-
-  var formatAuthors = function(authors) {
+    if (!authors) {
+      return ``;
+    }
     authors = splitAuthor(authors); //turn authors into an array of structured authors.
     authors.sort((a,b)=>{return b.lastName - a.lastName;});
-    //Do these two prior to passing to this function. Store authors[0].lastName to sort all of the citations.
 
-    stop = authors.length > 10 ? 7 : authors.length;
+    et_al = authors.length > 10;
+    stop = et_al ? 7 : authors.length;
 
     function lastFirst(author) {
-      return `${author.lastName}, ${author.firstName}`;
+      return `${author.lastName}${author.firstName ? `, ${author.firstName}` : ``}`; //For one word names, skip the first name.
     }
 
     function firstLast(author) {
-      return `${author.firstName} ${author.lastName}`;
+      return (`${author.firstName} ${author.lastName}`).trim();
     }
 
     function otherAuthors(authors) {
       return `${authors.map((author,index) =>
-        (index > 0 && index < stop) ? `, ${(index == stop - 1) ? 'and ' : ''}${firstLast(author)}` : ``
+        (index > 0 && index < stop) ? `, ${(index == stop - 1 && !et_al) ? 'and ' : ''}${firstLast(author)}${(index == stop - 1 && et_al) ? ' et al' : ''}` : ``
       ).join('')}`;
     }
 
-    return `${lastFirst(authors[0])}${authors.length > 1 ? `${otherAuthors(authors)}` : ''}`;
-  };
-
-
-
-  str += "\n";
-
-  var first = true;
-  var citekeys = {};
-  //var item;
-  //while(item = Zotero.nextItem()) {
-  var len = rows.length;
-  for (var i = 0; i < len; i++) {
-    var item = rows[i];
-    console.log('item ', i, item);
-    // determine type
-    var type = "misc";
-    //var type = zotero2bibtexTypeMap[item.itemType];
-    //if (typeof(type) == "function") { type = type(item); }
-    //if(!type) type = "misc";
-    item.creators = splitAuthor(item.author);
-    var date = splitDate(item.date);
-    item.year = date.year;
-    item.month = toMonths(date.month,"lower");
-    // create a unique citation key
-    var citekey = buildCiteKey(item, citekeys);
-
-    // write citation key
-    str += ((first ? "" : ",\n\n") + "@" + type + "{" + citekey);
-    first = false;
-
-    //No
-    if (item.reportNumber || item.issue || item.seriesNumber) {
-      writeField("number", item.reportNumber || item.issue || item.seriesNumber);
-    }
-
-    //No
-    if (item.publicationTitle) {
-      if (item.itemType == "bookSection" || item.itemType == "conferencePaper") {
-        writeField("booktitle", item.publicationTitle);
-      } else {
-        writeField("journal", item.publicationTitle);
-      }
-    }
-
-    //Perhaps in the future
-    if (item.publisher) {
-      if (item.itemType == "thesis") {
-        writeField("school", item.publisher);
-      } else if (item.itemType == "report") {
-        writeField("institution", item.publisher);
-      } else {
-        writeField("publisher", item.publisher);
-      }
-    }
-
-    if (item.title) {
-      writeField("title", item.title);
-    }
-
-    //Create new author function to split by ';' and reorder by last,first
-    //Result should be last,first and last,first
-
-    if (item.creators && item.creators.length) {
-      //console.log('creators ', item.creators);
-      // split creators into subcategories
-      var author = "";
-      var editor = "";
-      var translator = "";
-      for (var k in item.creators) {
-        var creator = item.creators[k]; //item{ creators{ creator{ firstName, lastName, creatorType } } }
-        var creatorString = creator.lastName;
-
-        if (creator.firstName) {
-          creatorString = creator.lastName + ", " + creator.firstName;
-        } else if (creator.fieldMode == true) { // fieldMode true, assume corporate author
-          creatorString = "{" + creator.lastName + "}";
-        }
-
-        if (creator.creatorType == "editor" || creator.creatorType == "seriesEditor") {
-          editor += " and " + creatorString;
-        } else if (creator.creatorType == "translator") {
-          translator += " and " + creatorString;
-        } else {
-          author += " and " + creatorString;
-        }
-      }
-
-      if (author) {
-        writeField("author", author.substr(5)); //Start at five to cut off the first 'and'.
-      }
-      if (editor) {
-        writeField("editor", editor.substr(5));
-      }
-      if (translator) {
-        writeField("translator", translator.substr(5));
-      }
-    }
-
-    if (item.month && item.year) {
-      //console.log('date ',item.month,item.year);
-      writeField("month", item.month);
-      writeField("year", item.year);
-    }
-
-    //???
-    if (item.summary) {
-      writeField("note", item.summary);
-    }
-
-    //Yes
-    if (item.tags) {
-      var tagString = "";
-      var tags = item.tags.split(" "); //when updating the tag functions fix this.
-      for (var l in tags) {
-        tagString += ", " + tags[l];
-      }
-      writeField("keywords", tagString.substr(2));
-    }
-
-    //No pages.
-    if (item.pages) {
-      writeField("pages", item.pages.replace("-", "--"));
-    }
-
-    //Yes
-    if (item.url) {
-      writeField("howpublished", item.url);
-    }
-
-    str += ("\n}");
+    return `${lastFirst(authors[0])}${authors.length > 1 ? `${otherAuthors(authors)}` : ''}.`;
   }
 
-  return str;
-}
+  function formatDate(date) {
+    //format dates: `Month DD, YYYY.`
+    date = splitDate(date);
+    return `${toMonths(date.month, 'long')} ${date.day ? `${date.day}, ` : ``}${date.year}`;
+  }
 
-//Use String.concat(string, string, ...
+  function formatTitle(title) {
+    return title ? `"${title}."` : '';
+  }
+
+  function formatURL(url) {
+    return url ? `${url}.` : '';
+  }
+
+  function formatCitation(item) {
+    switch(item.type) {
+      default:
+      return website(item.type, item.title, item.author, item.date, item.datepublished, item.url);
+    }
+  }
+
+  function sortAlpha(a, b){
+    first = a.split(' ')[0];
+    second = b.split(' ')[0];
+    if(first < second) { return -1; }
+    if(first > second) { return 1; }
+    return 0;
+  }
+
+  return `${rows.map((row) => `${formatCitation(row)}`).sort(sortAlpha).join('')}`;
+}
