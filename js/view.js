7eaca30ec403ba7183bac86b4c2c8e93cd61ba30
owenschoppe <owen.schoppe@gmail.com>
2019-02-25 12:42:22 -0800

    fix(view) Add polyfill

--- js/view.js
+++ js/view.js
@@ -714,3 +714,8 @@
     chrome.storage.local.get(null, onStorage);
   }
 })();
+
+//Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
