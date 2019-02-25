5c0ee2c0fd246ecc81ae07db2810c33ef4cc7dcd
owenschoppe <owen.schoppe@gmail.com>
2019-02-25 10:57:47 -0800

    fix(popup) Add polyfill

--- js/popup.js
+++ js/popup.js
@@ -1289,3 +1289,15 @@ Code may not be used without written and express permission.
 
   //End Closure
 })();
+
+//Polyfill
+if (!Object.entries)
+    Object.entries = function (obj) {
+        var ownProps = Object.keys(obj),
+            i = ownProps.length,
+            resArray = new Array(i); // preallocate the Array
+
+        while (i--)
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+        return resArray;
+    };
