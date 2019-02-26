89e77eeb9af07383225e0692b18f51b16ae31c0d
owenschoppe <owen.schoppe@gmail.com>
2019-02-25 21:02:28 -0800

    fix(templates) Styling and js

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-	console.log('open print dialog');
-	window.print();
-	console.log('done printing');
-	window.close();
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log('open print dialog');
+    window.print();
+    console.log('done printing');
+    window.close();
+});
