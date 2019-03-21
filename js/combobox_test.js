94a82f2b45fa03a0153c31d6a4120615adf98f08
owenschoppe <owen.schoppe@gmail.com>
2019-03-20 21:15:52 -0700

    fix(tags) cleanup dev files

--- js/combobox_test.js
+++
@@ -1,9 +0,0 @@
-var tag;
-
-document.addEventListener("DOMContentLoaded", () => {
-    tag = new TagInput('Tags','Enter a tag');
-    document.querySelector('#tagInput').appendChild(tag.div);
-    window.setTimeout(()=>{
-        tag.setTagOptions(['foo', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz', 'bar', 'baz']);
-    }, 100);
-});
