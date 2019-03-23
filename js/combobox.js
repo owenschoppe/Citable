b29ea0408330b875e1c164f32773ca22b4539aef
owenschoppe <owen.schoppe@gmail.com>
2019-03-23 10:24:47 -0700

    fix(tags) Dedupe options to prevent double tagging

--- js/combobox.js
+++ js/combobox.js
@@ -116,7 +116,7 @@ var Tags = (function () {
             //Should we only search if e.target.value.length > 2?
             var matching = [];
             for (var option of this.tagOptions) {
-                if (option.toLowerCase().includes(e.target.value.toLowerCase())) {
+                if (option.toLowerCase().includes(e.target.value.toLowerCase()) && !this.pillList.items.map(i => i.value).includes(option)) {
                     matching.push(option);
                 }
             }
