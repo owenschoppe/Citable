e7703a772d79d7b5d9d1c6ecd2fbfb0315611146
owenschoppe <owen.schoppe@gmail.com>
2019-03-22 18:00:48 -0700

    fix(tags) polyfill for NodeList.forEach

--- js/combobox.js
+++ js/combobox.js
@@ -410,3 +410,8 @@ var Tags = (function () {
         TagInput: TagInput
     };
 }());
+
+//Polyfill for es5
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
