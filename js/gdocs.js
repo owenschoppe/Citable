b29110a1a3affb05940d32aef9b0bf66f1d69888
Owen Schoppe <owen.schoppe@gmail.com>
2021-04-28 20:57:17 -0700

    Cross browser login
    Move authentication to use standard browser.identity apis using web app oauth2. Works in Brave and Vivaldi

--- js/gdocs.js
+++ js/gdocs.js
@@ -17,111 +17,258 @@ Author: Eric Bidelman (ericbidelman@chromium.org)
 */
 
 function GDocs(selector) {
-
-    var SCOPE_ = 'https://www.googleapis.com/drive/v2/';
+    var SCOPE_ = "https://www.googleapis.com/drive/v2/";
 
     this.lastResponse = null;
 
-    this.__defineGetter__('SCOPE', function () {
+    this.__defineGetter__("SCOPE", function () {
         return SCOPE_;
     });
 
-    this.__defineGetter__('DOCLIST_FEED', function () {
-        return SCOPE_ + 'files';
+    this.__defineGetter__("DOCLIST_FEED", function () {
+        return SCOPE_ + "files";
     });
 
-    this.__defineGetter__('CREATE_SESSION_URI', function () {
-        return 'https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable';
+    this.__defineGetter__("CREATE_SESSION_URI", function () {
+        return "https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable";
     });
 
-    this.__defineGetter__('DEFAULT_CHUNK_SIZE', function () {
+    this.__defineGetter__("DEFAULT_CHUNK_SIZE", function () {
         return 1024 * 1024 * 5; // 5MB;
     });
+
+    const REDIRECT_URL = browser.identity.getRedirectURL();
+    const { oauth2 } = browser.runtime.getManifest();
+    const CLIENT_ID = oauth2.client_id;
+    // const API_KEY = "AIzaSyAWw_FR8PovrAx8d4aJo2rJb3IY0Zfq91A";
+    const SCOPES = [
+        "openid",
+        "https://www.googleapis.com/auth/drive",
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+    ];
+    const AUTH_URL = `https://accounts.google.com/o/oauth2/auth\
+?client_id=${CLIENT_ID}\
+&response_type=token\
+&redirect_uri=${encodeURIComponent(REDIRECT_URL)}\
+&scope=${encodeURIComponent(SCOPES.join(" "))}`;
+    const VALIDATION_BASE_URL =
+        "https://www.googleapis.com/oauth2/v3/tokeninfo";
+    this.REVOKE_URL = (token) =>
+        `https://accounts.google.com/o/oauth2/revoke?token=${token}`;
+
+    function extractParam(redirectUri, key) {
+        let m = redirectUri.match(/[#?](.*)/);
+        if (!m || m.length < 1) return null;
+        let params = new URLSearchParams(m[1].split("#")[0]);
+        return params.get(key);
+    }
+
+    /**
+Validate the token contained in redirectURL.
+This follows essentially the process here:
+https://developers.google.com/identity/protocols/OAuth2UserAgent#tokeninfo-validation
+- make a GET request to the validation URL, including the access token
+- if the response is 200, and contains an "aud" property, and that property
+matches the clientID, then the response is valid
+- otherwise it is not valid
+
+Note that the Google page talks about an "audience" property, but in fact
+it seems to be "aud".
+*/
+    this.validate = (redirectURL) => {
+        console.log("redirectURL", redirectURL);
+
+        const accessToken = extractParam(redirectURL, "access_token");
+        if (!accessToken) {
+            throw "Authorization failure";
+        }
+        const validationURL = `${VALIDATION_BASE_URL}?access_token=${accessToken}`;
+        const validationRequest = new Request(validationURL, {
+            method: "GET",
+        });
+
+        function checkResponse(response) {
+            return new Promise((resolve, reject) => {
+                if (response.status != 200) {
+                    reject("Token validation error");
+                }
+                response.json().then((json) => {
+                    console.log("token info:", json);
+                    if (json.aud && json.aud === CLIENT_ID) {
+                        resolve({
+                            access_token: accessToken,
+                            exp: json.exp,
+                            expires_in: extractParam(redirectURL, "expires_in"),
+                            created: Date.now(),
+                        });
+                    } else {
+                        reject("Token validation error");
+                    }
+                });
+            });
+        }
+
+        return fetch(validationRequest).then(checkResponse);
+    };
+
+    /**
+Authenticate and authorize using browser.identity.launchWebAuthFlow().
+If successful, this resolves with a redirectURL string that contains
+an access token.
+*/
+    this.authorize = (interactive) => {
+        return browser.identity.launchWebAuthFlow({
+            interactive: interactive,
+            url: AUTH_URL,
+        });
+    };
 }
 
-GDocs.prototype.auth = function (interactive, opt_callback) {
-    try {
-        chrome.identity.getAuthToken({
-            interactive: interactive
-        }, function (token) {
-            if (token) {
-                this.accessToken = token;
-                console.log(token);
-            }
-            if (opt_callback) {
-                opt_callback(token);
-            }
-        }.bind(this));
-    } catch (e) {
-        console.log('Authorization Error', e);
-        if (opt_callback) {
+GDocs.prototype.auth = function (interactive, opt_callback = () => {}) {
+    // try {
+    //     chrome.identity.getAuthToken(
+    //         {
+    //             interactive: interactive,
+    //         },
+    //         function (token) {
+    //             if (token) {
+    //                 this.accessToken = token;
+    //                 console.log(token);
+    //             }
+    //             if (opt_callback) {
+    //                 opt_callback(token);
+    //             }
+    //         }.bind(this)
+    //     );
+    // } catch (e) {
+    //     console.log("Authorization Error", e);
+    //     if (opt_callback) {
+    //         opt_callback();
+    //     }
+    // }
+    this.authorize(interactive)
+        .then(this.validate)
+        .then((tokenObject) => {
+            this.accessToken = tokenObject.access_token;
+            this.expiration = tokenObject.exp;
+            return this.accessToken;
+        })
+        .then(opt_callback)
+        .catch((error) => {
+            console.log("Authorization Error", error);
             opt_callback();
-        }
-    }
+        });
 };
 
-GDocs.prototype.removeCachedAuthToken = function (opt_callback) {
-    if (this.accessToken) {
-        var accessToken = this.accessToken;
-        this.accessToken = null;
-        // Remove token from the token cache.
-        chrome.identity.removeCachedAuthToken({
-            token: accessToken
-        }, function () {
-            if (opt_callback) {
-                opt_callback();
-            }
-        });
-    } else {
-        if (opt_callback) {
+GDocs.prototype.removeCachedAuthToken = function (opt_callback = () => {}) {
+    // if (this.accessToken) {
+    //     var accessToken = this.accessToken;
+    //     this.accessToken = null;
+    //     // Remove token from the token cache.
+    //     chrome.identity.removeCachedAuthToken(
+    //         {
+    //             token: accessToken,
+    //         },
+    //         function () {
+    //             if (opt_callback) {
+    //                 opt_callback();
+    //             }
+    //         }
+    //     );
+    // } else {
+    //     if (opt_callback) {
+    //         opt_callback();
+    //     }
+    // }
+    this.authorize(false)
+        .then(this.validate)
+        .then((tokenObject) => {
+            const revokeRequest = new Request(
+                this.REVOKE_URL(tokenObject.access_token),
+                { method: "POST" }
+            );
+            return fetch(revokeRequest).then((response) => response.ok);
+        })
+        .then((ok) => {
+            this.accessToken = null;
             opt_callback();
-        }
-    }
+        })
+        .catch((error) => {
+            console.log("Revocation Error", error);
+            opt_callback();
+        });
 };
 
 GDocs.prototype.revokeAuthToken = function (opt_callback) {
     if (this.accessToken) {
-        // Make a request to revoke token
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
-            this.accessToken);
-        xhr.send();
-        this.removeCachedAuthToken(opt_callback);
+        // // Make a request to revoke token
+        // var xhr = new XMLHttpRequest();
+        // xhr.open(
+        //     "GET",
+        //     "https://accounts.google.com/o/oauth2/revoke?token=" +
+        //         this.accessToken
+        // );
+        // xhr.send();
+        // this.removeCachedAuthToken(opt_callback);
+        this.authorize(false)
+            .then(this.validate)
+            .then((tokenObject) => {
+                const revokeRequest = new Request(
+                    this.REVOKE_URL(tokenObject.access_token),
+                    { method: "POST" }
+                );
+                return fetch(revokeRequest).then((response) => response.ok);
+            })
+            .then((ok) => {
+                this.accessToken = null;
+                opt_callback();
+            })
+            .catch((error) => {
+                console.log("Revocation Error", error);
+                opt_callback();
+            });
     }
 };
 
 /*
  * Generic HTTP AJAX request handler.
  */
-GDocs.prototype.makeRequest = function (method, url, callback, opt_data, opt_headers) {
+GDocs.prototype.makeRequest = function (
+    method,
+    url,
+    callback,
+    opt_data,
+    opt_headers
+) {
     var data = opt_data || null;
     var headers = opt_headers || {};
 
     var xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
 
-
-
     // Include common headers (auth and version) and add rest.
-    xhr.setRequestHeader('Authorization', 'Bearer ' + this.accessToken);
+    xhr.setRequestHeader("Authorization", "Bearer " + this.accessToken);
     for (var key in headers) {
         xhr.setRequestHeader(key, headers[key]);
     }
 
     xhr.onload = function (e) {
-        console.log('onload', this, e.target);
+        console.log("onload", this, e.target);
         this.lastResponse = this.response;
         callback(e.target.response, e.target);
     }.bind(this);
     xhr.onerror = function (e) {
-        console.log(this, this.status, this.response,
-            this.getAllResponseHeaders());
+        console.log(
+            this,
+            this.status,
+            this.response,
+            this.getAllResponseHeaders()
+        );
     };
-    console.log('send data', xhr);
+    console.log("send data", xhr);
     xhr.send(data);
 };
-
-
 
 /**
  * Uploads a file to Google Docs.
@@ -165,7 +312,7 @@ GDocs.prototype.makeRequest = function (method, url, callback, opt_data, opt_hea
 GDocs.handleError = function (xhr, textStatus) {
     //util.hideMsg();
     if (xhr.status != 0) {
-        Util.displayError(xhr.status, ' ', xhr.statusText);
+        Util.displayError(xhr.status, " ", xhr.statusText);
     } else {
         Util.displayError("No internet connection.");
     }
--- js/options.js
+++ js/options.js
@@ -1,14 +1,20 @@
 (function () {
     var bgPage = chrome.extension.getBackgroundPage();
 
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelector('#revoke').addEventListener('click', revokeToken);
-        document.querySelector('#donate').addEventListener('click', donate);
-        document.querySelector('#view_policy').addEventListener('click', policy);
-        document.querySelector('#analytics').addEventListener('change', analytics);
-        document.getElementById('configure').addEventListener('click', (e) => {
+    document.addEventListener("DOMContentLoaded", function () {
+        document
+            .querySelector("#revoke")
+            .addEventListener("click", revokeToken);
+        document.querySelector("#donate").addEventListener("click", donate);
+        document
+            .querySelector("#view_policy")
+            .addEventListener("click", policy);
+        document
+            .querySelector("#analytics")
+            .addEventListener("change", analytics);
+        document.getElementById("configure").addEventListener("click", (e) => {
             chrome.tabs.create({
-                url: "chrome://extensions/configureCommands"
+                url: "chrome://extensions/configureCommands",
             });
         });
         initUI();
@@ -16,55 +22,73 @@
     });
 
     function revokeToken() {
-        ga('send', 'event', 'Button', 'Revoke Token');
+        ga("send", "event", "Button", "Revoke Token");
 
-        chrome.identity.getAuthToken({
-                'interactive': true
-            },
-            function (current_token) {
-                if (!chrome.runtime.lastError) {
-                    // Remove the local cached token
-                    chrome.identity.removeCachedAuthToken({
-                            token: current_token
-                        },
-                        function () {});
+        // chrome.identity.getAuthToken(
+        //     {
+        //         interactive: true,
+        //     },
+        //     function (current_token) {
+        //         if (!chrome.runtime.lastError) {
+        //             // Remove the local cached token
+        //             chrome.identity.removeCachedAuthToken(
+        //                 {
+        //                     token: current_token,
+        //                 },
+        //                 function () {}
+        //             );
 
-                    // Make a request to revoke token in the server
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
-                        current_token);
+        //             // Make a request to revoke token in the server
+        //             var xhr = new XMLHttpRequest();
+        //             xhr.open(
+        //                 "GET",
+        //                 "https://accounts.google.com/o/oauth2/revoke?token=" +
+        //                     current_token
+        //             );
 
+        //             xhr.onload = function (e) {
+        //                 console.log("onload", this, e.target);
+        //                 this.lastResponse = this.response;
+        //             }.bind(this);
 
-                    xhr.onload = function (e) {
-                        console.log('onload', this, e.target);
-                        this.lastResponse = this.response;
-                    }.bind(this);
+        //             xhr.onerror = function (e) {
+        //                 console.log(this, this.status, this.response);
+        //             };
 
-                    xhr.onerror = function (e) {
-                        console.log(this, this.status, this.response);
-                    };
+        //             xhr.send();
 
-                    xhr.send();
-
-                    document.getElementById('revoke').disabled = true;
-                    console.log('Token revoked and removed from cache. ' +
-                        'Check chrome://identity-internals to confirm.');
-                    initUI();
-                }
-            });
+        //             document.getElementById("revoke").disabled = true;
+        //             console.log(
+        //                 "Token revoked and removed from cache. " +
+        //                     "Check chrome://identity-internals to confirm."
+        //             );
+        //             initUI();
+        //         }
+        //     }
+        // );
+        bgPage.gdocs.revokeAuthToken(function () {
+            document.getElementById("revoke").disabled = true;
+            console.log(
+                "Token revoked and removed from cache. " +
+                    "Check chrome://identity-internals to confirm."
+            );
+            initUI();
+        });
     }
 
     function donate(e) {
-        ga('send', 'event', 'Button', 'Donate', 'Options');
+        ga("send", "event", "Button", "Donate", "Options");
     }
 
     function policy(e) {
-        ga('send', 'event', 'Button', 'View Policy');
+        ga("send", "event", "Button", "View Policy");
     }
 
     function initCommands() {
         chrome.commands.getAll((all) => {
-            document.getElementById('browser_action').innerText = browserAction(all);
+            document.getElementById("browser_action").innerText = browserAction(
+                all
+            );
         });
     }
 
@@ -79,35 +103,33 @@
 
     function initAnalytics(response) {
         var input = document.querySelector("#analytics");
-        input.checked = response.analytics == undefined ? true : response.analytics;
+        input.checked =
+            response.analytics == undefined ? true : response.analytics;
         input.disabled = false;
     }
 
     function analytics(e) {
         chrome.storage.sync.set({
-            analytics: e.target.checked
+            analytics: e.target.checked,
         });
     }
 
     function initUI() {
-        chrome.identity.getAuthToken({
-            'interactive': false
-        }, function (token) {
+        bgPage.toggleAuth(false, function (token) {
             // Use the token.
             if (!token) {
                 //if (!bgPage.oauth.hasToken()) {
-                document.getElementById('revoke').disabled = true;
+                document.getElementById("revoke").disabled = true;
             } else {
-                console.log('Success!');
-                access_token = token;
-                document.getElementById('revoke').disabled = false;
+                console.log("Success!");
+                // access_token = token;
+                document.getElementById("revoke").disabled = false;
             }
-            if (chrome.runtime.lastError) {
-                console.log('Oops', chrome.runtime.lastError);
-            }
+            // if (chrome.runtime.lastError) {
+            //     console.log("Oops", chrome.runtime.lastError);
+            // }
 
-            chrome.storage.sync.get('analytics', initAnalytics);
-
+            chrome.storage.sync.get("analytics", initAnalytics);
         });
     }
 })();
